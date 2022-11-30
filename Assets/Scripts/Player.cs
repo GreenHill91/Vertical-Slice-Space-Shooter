bb46c08831a0d2fce7b9bf0699c909768ed30a3a
BlakeClarkeSAE <115077848+BlakeClarkeSAE@users.noreply.github.com>
2022-11-30 12:57:23 +0000

    player movement
    new controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,39 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody rb;
-    private float movementX;
-    private float movementY;
+    public Rigidbody2D rb;
+    public float speed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-
-        movementX = movementVector.x;
-        movementY = movementVector.y;
-    }
+    private Vector2 keyMovement;
 
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        keyMovement.x = Input.GetAxisRaw("Horizontal");
+        keyMovement.y = Input.GetAxisRaw("Vertical");
 
-        rb.AddForce(movement);
-    }
+        Debug.Log(keyMovement);
 
+        transform.Translate(-keyMovement * speed);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
